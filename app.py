17fd418138652e35cde214c57eda48db23af7258
MrDVolk <37294024+MrDVolk@users.noreply.github.com>
2024-04-07 18:51:55 +0000

    SK experiments in progress

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from langchain.memory import ChatMessageHistory
+from semantic_kernel.contents.chat_history import ChatHistory
 from src.actors.actor import Actor
 import chainlit as cl
 
 @cl.on_chat_start
 async def on_chat_start():
     cl.user_session.set("actor", Actor(config={}))
-    cl.user_session.set("history", ChatMessageHistory())
+    cl.user_session.set("history", ChatHistory())
 
 
 @cl.on_message
@@ -15,6 +15,6 @@ async def on_message(message: cl.Message):
     question = message.content
     history.add_user_message(question)
     answer = actor.generate_answer(question, history)
-    history.add_ai_message(answer)
+    history.add_assistant_message(answer)
     msg = cl.Message(content=answer)
     await msg.send()
